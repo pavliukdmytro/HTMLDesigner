eb6fd0ea2c772ca6e129a6db01d4fb95431f3728
Dima Pavliuk <Pavliuk.Dmytro@aniart.com.ua>
2023-08-28 14:14:46 +0300

    created buttons

--- src/grapes/blocks/buttons/buttons.ts
+++ src/grapes/blocks/buttons/buttons.ts
@@ -9,7 +9,17 @@ export default [
     label: `<div>Button primary</div>`,
     type: 'link',
     content: `
-        <a class="g-button-primary" data-gjs-type="link" data-gjs-droppable="true">asdfasdf</a>
+        <a class="g-button-primary" data-gjs-type="link" data-gjs-droppable="true">Button primary</a>
+    `,
+    style: {},
+  },
+  {
+    id: 'button-secondary', // id is mandatory
+    category,
+    label: `<div>Button secondary</div>`,
+    type: 'link',
+    content: `
+        <a class="g-button-secondary" data-gjs-type="link" data-gjs-droppable="true">Button secondary</a>
     `,
     style: {},
   },
