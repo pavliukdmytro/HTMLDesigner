b70c7ea4433458adc8938f1a5a819f6d3cc9898c
Pavliuk Dmytro <smileua18@gmail.com>
2024-03-13 17:36:15 +0200

    master - added text block

--- src/grapes/blocks/text/text.ts
+++ src/grapes/blocks/text/text.ts
@@ -1,6 +1,7 @@
 import link from '@/grapes/blocks/text/link/link';
 import type { BlockProperties } from 'grapesjs';
+import textBlock from '@/grapes/blocks/text/textBlock/textBlock';
 
-const textBlockArray: Array<BlockProperties> = [link];
+const textBlockArray: Array<BlockProperties> = [link, textBlock];
 
 export default textBlockArray;
--- src/grapes/components/index.ts
+++ src/grapes/components/index.ts
@@ -1,4 +1,5 @@
 import gridContainerComponent from '@/grapes/components/grid/grid-container/grid-container';
 import gridItemComponent from '@/grapes/components/grid/grid-item/grid-item';
+import textBlockComponent from '@/grapes/components/text/textBlock/textBlock';
 
-export default [gridContainerComponent, gridItemComponent];
+export default [gridContainerComponent, gridItemComponent, textBlockComponent];
